90110efa33d4d348f6d1c24b04ffedca9dca9151
eunbinoh <nohonbi01@gmail.com>
2022-04-19 19:33:41 +0900

    appConfig 리팩토링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,11 +11,21 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(MemoryMemberRepository());
 	}
 	
+	//중복 리팩토링
+	private MemberRepository MemoryMemberRepository() {
+		return new MemoryMemberRepository();
+	}
+
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(MemoryMemberRepository(), discountPolicy());
+	}
+	
+	//리팩토링 (기능 분리 구현, 명확한 명명, 호출)
+	public DiscountPolicy discountPolicy() {
+		return new FixDiscountPolicy();
 	}
 	
 	
